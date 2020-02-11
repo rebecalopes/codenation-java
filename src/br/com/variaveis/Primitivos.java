99c9b28c1a709a3d1b2c3487a799bc2ef1797756
rebecalopes <rebeca.lopes.141@gmail.com>
2020-02-10 22:01:56 -0300

    tipos de variaveis

---
+++ src/br/com/variaveis/Primitivos.java
@@ -0,0 +1,26 @@
+package br.com.variaveis;
+
+public class Primitivos {
+	
+	byte byteVar = 127;
+	
+	short curto = 10000;
+	Short curtoWrapper = 1000;
+	
+	int inteiro = 1000000000;
+	Integer inteiroWrapper = 1000000;
+	
+	long longo = 100000000000000000L;
+	
+	float pontoFlutuante = 10.10f;
+	
+	double pontoFlutuanteDobro = 100000.299;
+	
+	char character = 'r';
+	
+	boolean booleano = false;
+	
+	
+	
+
+}
